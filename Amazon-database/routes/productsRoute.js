a3af1687ab2ef5f2780ab314173497d73fa4d3db
Akshit Bansal <130559526+Akshit191002@users.noreply.github.com>
2024-04-27 20:12:57 +0530

    Delete Amazon-database directory

--- Amazon-database/routes/productsRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const productController=require('../Controller/productController.js')
-const productRouter=express.Router();
-productRouter.route('/')
-    .get(productController.getAllProduct)
-    // .post(productController.addProduct)
-module.exports=productRouter;
